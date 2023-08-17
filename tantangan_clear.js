d3952c1c52a553a7b62694cab9b9a2f62649250a
RahmanIslamIen <rahmanislamien@gmail.com>
2023-08-17 10:54:39 +0700

    first commit

---
+++ tantangan_clear.js
@@ -0,0 +1,14 @@
+const initialMemoryUsage = process.memoryUsage().heapUsed;
+const yourName = "rahman islam";
+const environment = "development";
+
+for (let i = 0; i <= 10000; i++) {
+  // Proses looping ini akan membuat penggunaan memori naik
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`);
+console.log(
+  `Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`
+);
